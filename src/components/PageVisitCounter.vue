08937352cbd385a88bf9ad6fb91d1ef900e7cd72
arthur.pozhidaev <>
2021-11-24 14:04:12 +0700

    tested

--- src/components/PageVisitCounter.vue
+++ src/components/PageVisitCounter.vue
@@ -11,7 +11,7 @@ import { Timestamp } from "firebase/firestore";
 export default defineComponent({
   name: "PageVisitCounter",
   async setup() {
-    
+    console.log("setupped")
     window.visitCounterLibrary.initialize =
       async (
         pageVisitedEventName: string,
@@ -19,6 +19,7 @@ export default defineComponent({
         entitiesToTrack: string[],
         storageName: string
       ) => {
+        console.log("initialized")
         if (window.visitCounterLibrary.initTime == null) {
           const storage = await getRemoteStorage(storageName);
           initializesessionStorage(storageName, storage);
